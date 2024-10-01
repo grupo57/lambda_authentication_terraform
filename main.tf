e50217f7a37cb44966fcb465f399ef744bf38d1e
grupo57 <grupo57fiap@gmail.com>
2024-10-01 18:00:18 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "environment" {
+  description = "The environment to deploy (dev or prod)"
+  type        = string
+}
+
+# Criação do bucket S3
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = "lambda-authentication-${var.environment}"
+}
+
+# Upload do arquivo JAR para o bucket S3
+resource "aws_s3_object" "lambda_jar" {
+  bucket = aws_s3_bucket.lambda_bucket.bucket
+  key    = "LambdaJwtCreator-6.0.0.jar"
+  source = "LambdaJwtCreator-6.0.0.jar"
+  etag   = filemd5("LambdaJwtCreator-6.0.0.jar")
+}
+
+# Função Lambda
+resource "aws_lambda_function" "auth_function" {
+  function_name = "lambda_authentication_${var.environment}"
+  handler       = "com.example.Handler"
+  runtime       = "java11"
+  role          = aws_iam_role.lambda_exec_role.arn
+  s3_bucket     = aws_s3_bucket.lambda_bucket.bucket
+  s3_key        = aws_s3_object.lambda_jar.key
+  source_code_hash = filebase64sha256("LambdaJwtCreator-6.0.0.jar")
+
+  environment {
+    variables = {
+      ENV = var.environment
+    }
+  }
+}
+
+# Role IAM para a função Lambda
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role_${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Anexar política à role IAM
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
